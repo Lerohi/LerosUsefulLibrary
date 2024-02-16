84eda47c9c3e9bb1d0f3a9f168e9e7abf8a1679e
lerohi <57595180+Lerohi@users.noreply.github.com>
2024-02-16 21:58:14 +0100

    ~ Rewrote some comments and reformatted code to comply with code conventions and optimize it

--- FileEditor.java
+++ FileEditor.java
@@ -5,87 +5,64 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * @author Lerohi
- * @version 20.0828
+ * @version 24.0216
  */
-public class FileEditor {
+public abstract class FileEditor {
 
-////////////////////////////////Constructor////////////////////////////////
-
-	public FileEditor() {
-		
+	private FileEditor() {
+		throw new IllegalStateException("This is a static utility class, therefore it can't be instantiated!");
 	}
-
-///////////////////////////////Main-Methods////////////////////////////////
 
 	/**
 	 * Appends the given text to the content of the given file
 	 * 
-	 * @param file		An Object of the class "File" that represents the file
-	 * @param text		A String containing the text that is supposed to be appended
+	 * @param file An Object of the class "File" that represents the file
+	 * @param text A String containing the text that is supposed to be appended
 	 */
-	public void append(File file, String text) {
-
-		ensureFileExistance(file);
-
-		try {
-
-			FileWriter fw = new FileWriter(file, true);
+	public static void append(File file, String text) {
+		ensureFileExistence(file);
+		try(FileWriter fw = new FileWriter(file, true)) {
 			fw.write(text);
 			fw.flush();
-			fw.close();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	/**
 	 * Overwrites the content of the given file with the given text
 	 * 
-	 * @param file		An Object of the class "File" that represents the file
-	 * @param text		A String containing the text that is supposed to be appended
+	 * @param file An Object of the class "File" that represents the file
+	 * @param text A String containing the text that is supposed to be appended
 	 */
-	public void overwrite(File file, String text) {
-
-		ensureFileExistance(file);
-
-		try {
-
-			FileWriter fw = new FileWriter(file, false);
+	public static void overwrite(File file, String text) {
+		ensureFileExistence(file);
+		try(FileWriter fw = new FileWriter(file, false)) {
 			fw.write(text);
 			fw.flush();
-			fw.close();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	/**
 	 * Reads the content of a file and returns it as a String
 	 * 
-	 * @param file 		An Object of the class "File" that represents the file
-	 * @return 			A String containing the content of the file that is being read
+	 * @param file An Object of the class "File" that represents the file
+	 * @return A String containing the content of the file that is being read
 	 */
-	public String read(File file) {
-
+	public static String read(File file) {
 		String res = "";
-		ensureFileExistance(file);
-
-		try {
-
-			Scanner sc = new Scanner(file);
+		ensureFileExistence(file);
+		try(Scanner sc = new Scanner(file)) {
 			while (sc.hasNext()) {
-				res = res + sc.next();
+				res += sc.next();
 			}
-			sc.close();
-
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -95,23 +72,16 @@ public class FileEditor {
 	/**
 	 * Reads the content of a file as lines and returns it as an ArrayList containing the lines as Strings
 	 * 
-	 * @param file 		An Object of the class "File" that represents the file
-	 * @return 			An ArrayList containing the lines of the file that is being read as Strings
+	 * @param file An Object of the class "File" that represents the file
+	 * @return An ArrayList containing the lines of the file that is being read as Strings
 	 */
-	public ArrayList<String> readLines(File file) {
-
+	public static List<String> readLines(File file) {
 		ArrayList<String> res = new ArrayList<>();
-		ensureFileExistance(file);
-
-		try {
-
-			Scanner sc = new Scanner(file);
+		ensureFileExistence(file);
+		try(Scanner sc = new Scanner(file)) {
 			while(sc.hasNextLine()) {
 				res.add(sc.nextLine());
 			}
-			
-			sc.close();
-
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -119,41 +89,30 @@ public class FileEditor {
 	}
 	
 	/**
-	 * 
 	 * Reads the content of a file and returns it as a String
 	 * 
-	 * @param file		An Object of the class "File" that represents the file
-	 * @param line		The number of the line that is supposed to be read
-	 * @return			A String containing the content of the line that is being read
+	 * @param file An Object of the class "File" that represents the file
+	 * @param line The number of the line that is supposed to be read
+	 * @return A String containing the content of the line that is being read
 	 */
-	public String readSpecificLine(File file, int line) {
-
-		String res = "";
-		ArrayList<String> lines = readLines(file);
-		res = lines.get(line);
-		return res;
-		
+	public static String readSpecificLine(File file, int line) {
+		List<String> lines = readLines(file);
+		return lines.get(line);
 	}
 
-//////////////////////////////Support-Methods//////////////////////////////
-
+	//Support-Methods
 	/**
 	 * Ensures that the given file exists, if it doesn't, it will create a new file with the properties defined in the given "File"-Object 
 	 * 
-	 * @param file		An Object of the class "File" that represents the file
+	 * @param file An Object of the class "File" that represents the file
 	 */
-	public void ensureFileExistance(File file) {
-
-		if (!file.exists()) {
-
+	public static void ensureFileExistence(File file) {
+		if(!file.exists()) {
 			try {
 				file.createNewFile();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
 		}
-
 	}
-
 }
